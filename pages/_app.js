887c12fc98d4ef94fd735ddd3c4c93fbf5f96881
Ahzam Khan <ahzam.main@gmail.com>
2023-04-18 11:52:53 +0530

    new vibes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,43 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+// pages/_app.js
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+          <Head>
+        <title>pakhi.fun</title>
+        <link rel="shortcut icon" href="pakhi.png" />
+        <meta
+          name="description"
+          content="crafting the craft — engg + design"
+          key="desc"
+        />
+        <meta property="og:title" content="pakhi" />
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/PFtW6Fy/enhanced-1.png"
+        />
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://pakhi.fun/" key="twt-url" />
+        <meta name="twitter:title" content="pakhi" key="twt-title" />
+        <meta
+          name="twitter:description"
+          content="crafting the craft — engg + design"
+          key="twt-desc"
+        />
+        <meta
+          name="twitter:image"
+          content="https://i.ibb.co/PFtW6Fy/enhanced-1.png"
+          key="twt-img"
+        />
+      </Head>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+    </>
+  );
 }
+
+export default MyApp;
